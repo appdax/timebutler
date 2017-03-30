737f23abf6960aadecdb1514a50a77f87876ea6c
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-03-30 10:29:37 +0200

    Fix codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,10 @@
 require 'simplecov'
-require 'codeclimate-test-reporter'
 require 'timecop'
 require 'pry'
-
-CodeClimate::TestReporter.start
 
 SimpleCov.start do
   add_filter '/spec'
   add_filter '/lib/extensions/exception_notifier.rb'
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      CodeClimate::TestReporter::Formatter
-    ]
-  )
 end
 
 Dir['lib/**/*.rb'].each { |f| require_relative "../#{f}" }
